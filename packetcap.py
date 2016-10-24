3d901834526fb1660a94b796b6a35f5daec57101
Shane Miller <miller-s5@email.ulster.ac.uk>
2016-10-24 14:21:21 +0100

    Update

--- packetcap.py
+++ packetcap.py
@@ -10,7 +10,7 @@ try:
 except socket.error:
     print('Socket could not be created.')
     sys.exit()
-outputFile = open('proxylelouetfr.csv', 'w', newline='')
+outputFile = open('proxycap.csv', 'w', newline='')
 writer = csv.writer(outputFile)
 # Write out the top row
 writer.writerow(['Version', 'Protocol', 'TTL', 'SrcAddr', 'DestAddr',
